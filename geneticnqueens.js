dfb4011b6faf7dffd3ffef9ea34522ba0aa62816
Tucker Joseph <rtjoseph11@gmail.com>
2013-05-11 00:04:41 -0700

    WIP genetic portion of genQueens is finished. majorDiag check not working.

--- geneticnqueens.js
+++ geneticnqueens.js
@@ -1,82 +1,116 @@
-GeneticNQueens = function(n){
-
-this.createBoard = function(){
-  var board = {layout: [], conflicts: 0};
-  colHash = {};
-  majorDiagHash = {};
-  minorDiagHash = {};
-  for (var i = 0; i < n; i++){
-    var colIndex = Math.floor(Math.random()*n);
-    board['layout'].push(colIndex);
-    colHash[colIndex] = !colHash[colIndex] ? 1 : colHash[colIndex] + 1;
-    var minorIndex = board['layout'].length + colIndex - 1;
-    var majorIndex = board['layout'].length - colIndex - 1;
-    minorDiagHash[minorIndex] = !minorDiagHash[minorIndex] ? 1 : minorDiagHash[minorIndex] + 1;
-    majorDiagHash[majorIndex] = !majorDiagHash[majorIndex] ? 1 : majorDiagHash[majorIndex] + 1;
-  }
-  board['conflicts'] += _(colHash).reduce(function(memo, item){
-    if (item > 1){
-      return memo + item -1;
-    } else{
-      return memo;
-    }
-  },0);
-    board['conflicts'] += _(majorDiagHash).reduce(function(memo, item){
-    if (item > 1){
-      return memo + item -1;
-    } else{
-      return memo;
-    }
-  },0);
-   board['conflicts'] += _(minorDiagHash).reduce(function(memo, item){
-    if (item > 1){
-      return memo + item -1;
-    } else{
-      return memo;
-    }
-  },0);
-  return board;
+var expDist = function(population){
+  var rand = Math.random() * 0.77;
+  var lambda = 1.5;
+  return Math.floor(population.length * Math.log(1-rand,Math.E) / (-lambda));
 };
 
-this.createPopulation = function(){
-  var array = [];
-  _.each(_.range(10*n), function(){
-    array.push(this.createBoard());
-  },this);
-  array.sort(this.compare);
-  array.reverse();
-  return array;
+var GeneticNQueens = function(n){
+  this.n = n;
+  this.population = this.createPopulation();
 };
-this.compare = function(a,b) {
+
+GeneticNQueens.prototype.createPopulation = function(){
+  var compare = function(a,b) {
   if (a.conflicts < b.conflicts)
      return -1;
   if (a.conflicts > b.conflicts)
     return 1;
   return 0;
+  };
+  var array = [];
+  _.each(_.range(10 * this.n), function(){
+    array.push(new Board(this.n));
+  },this);
+  array.sort(this.compare);
+  array.reverse();
+  return array;
 };
 
-this.prunePopulation = function(){
+GeneticNQueens.prototype.prunePopulation = function(){
   var deadIndices = [];
-  var lambda = 1.5;
-  for (var i = 0; i < Math.floor(population.length/4); i++){
-    var rand = Math.random()*.77;
-    var expDist = Math.log(1-rand,Math.E) / (-lambda);
-    deadIndices.push(Math.floor(expDist * population.length));
+  for (var i = 0; i < Math.floor(10 * this.n / 4); i++){
+    deadIndices.push(expDist(this.population));
   }
-  console.log(deadIndices);
+  _(deadIndices).each(function(indexVal){
+    this.population.splice(indexVal,1);
+  }, this);
 };
 
-this.matepopulation = function(){
-
+GeneticNQueens.prototype.mateBoards = function(board1, board2){
+   var layout = [];
+   layout = _(board1['layout']).map(function(gene, index){
+    var rand = Math.random();
+    return rand > 0.5 ? gene : board2['layout'][index];
+   });
+   return new Board(this.n,layout);
 };
 
-var population = this.createPopulation();
+GeneticNQueens.prototype.matePopulation = function(){
+  var mates = [];
+  for (var i = 0; i < 10 * this.n; i++){
+    mates[i] = [expDist(this.population), expDist(this.population)];
+  }
+  var newPopulation = _(mates).map(function(pair){
+    return this.mateBoards(this.population[pair[0]], this.population[pair[1]]);
+  }, this);
+  return newPopulation;
+};
 
-var index = 0;
+GeneticNQueens.prototype.evolve = function(){
+  var index = 0;
+  while(this.population[0]['conflicts'] > 0 && index < 1000){
+    this.prunePopulation();
+    this.population = this.matePopulation();
+    index++;
+  }
+  return this.population[0];
+};
 
-// while(population[0]['conflicts'] > 0 && index < 1000){
-//   this.prunePopulation();
-//   this.matePopulation();
-// }
-
+var Board = function(n, array){
+  this.layout = array ? array : this.seedBoard(n);
+  this.conflicts = 0;
+  this.colHash = {};
+  this.majorDiagHash = {};
+  this.minorDiagHash = {};
+  this.generateHashses();
+  this.conflics = this.calculateConflicts();
+};
+Board.prototype.seedBoard = function(n){
+  var result = [];
+  for (var i = 0; i < n; i++){
+    result.push(Math.floor(Math.random()*n));
+  }
+  return result;
+};
+Board.prototype.generateHashses = function(){
+  _(this.layout).each(function(colIndex){
+    var minorIndex = this.layout.length + colIndex - 1;
+    var majorIndex = this.layout.length - colIndex - 1;
+    this.colHash[colIndex] = !this.colHash[colIndex] ? 1 : this.colHash[colIndex] + 1;
+    this.minorDiagHash[minorIndex] = !this.minorDiagHash[minorIndex] ? 1 : this.minorDiagHash[minorIndex] + 1;
+    this.majorDiagHash[majorIndex] = !this.majorDiagHash[majorIndex] ? 1 : this.majorDiagHash[majorIndex] + 1;
+  }, this);
+};
+Board.prototype.calculateConflicts = function(){
+  this.conflicts += _(this.colHash).reduce(function(memo, item){
+    if (item > 1){
+      return memo + item -1;
+    } else{
+      return memo;
+    }
+  },0);
+    this.conflicts += _(this.majorDiagHash).reduce(function(memo, item){
+    if (item > 1){
+      return memo + item -1;
+    } else{
+      return memo;
+    }
+  },0);
+   this.conflicts += _(this.minorDiagHash).reduce(function(memo, item){
+    if (item > 1){
+      return memo + item -1;
+    } else{
+      return memo;
+    }
+  },0);
 };
